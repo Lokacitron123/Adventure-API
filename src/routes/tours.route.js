9bc76ab0312825725ae6c5935af12e5b79009adb
Johan Lindell <johan.lindell91@gmail.com>
2025-09-28 15:39:20 +0200

    cleaned up tours route

--- src/routes/tours.route.js
+++ src/routes/tours.route.js
@@ -6,7 +6,6 @@ import {
   postTour,
   updateTour,
   deleteTour,
-  checkBody,
   aliasTopTours,
   getTourStats,
   getMonthlyPlan,
@@ -26,21 +25,6 @@ router.param("id", (req, res, next, val) => {
   req.tourId = val;
   next();
 });
-
-// Body Checker
-// router.use("/", (req, res, next) => {
-//   if (req.method === "POST" || req.method === "PATCH" || req.method === "PUT") {
-//     return checkBody(req, res, next);
-//   }
-//   next();
-// });
-
-// Same as below
-// router.get("/", getTours);
-// router.get("/:id", getTour);
-// router.put("/:id", putTour);
-// router.post("/", postTour);
-// router.delete("/:id", deleteTour);
 
 router.route("/top-5-cheap").get(aliasTopTours, getTours);
 router.route("/tour-stats").get(getTourStats);
